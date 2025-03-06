7994ba49c6a92d080140ef99a833e0a03e9e034a
craigblanch <craig41bl@gmail.com>
2025-03-06 16:59:30 -0700

    sending up for tests

---
+++ aya_eval.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=aya_eval
+#SBATCH --account soc-gpu-np
+#SBATCH --partition soc-gpu-np
+#SBATCH --gres=gpu
+#SBATCH --requeue
+#SBATCH --mail-user=u0013114@utah.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=outputs-%j
+#SBATCH --nodes=1
+#SBATCH --mem=245GB
+#SBATCH --ntasks=1
+#SBATCH --time=5:00:00
+
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+
+WORKDIR=/scratch/general/vast/$USER/CondaQA_Private
+OUTDIR=$WORKDIR/src/eval/chat-eval/llama3-3-70B/gen_edits_gen_qs/
+source /uufs/chpc.utah.edu/common/home/u0013114/.venv/bin/activate
+module load cuda/12.4.0
+
+nvidia-smi
+python -c "import torch; torch.cuda.is_available()"
+echo "SLURM JOB ID: $SLURM_JOBID"
+
+mkdir -p /scratch/general/vast/$USER/huggingface_cache
+mkdir -p $OUTDIR
+export HF_HOME="/scratch/general/vast/$USER/huggingface_cache"
+
+
+#python condaqa_eval.py --test_file $WORKDIR/data/final_condaqa_labeled/gen_edits_gen_qs_full.json --output_dir $OUTDIR --model_name llama3_3_70b --model_type huggingface --hk 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
+python aya_eval.py
--- aya_example.py
+++ aya_example.py
@@ -22,43 +22,68 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     aya_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
-    in_file = "data/encoded/D1_5_word_groups_2.txt"
+    in_file = "data/encoded/D1_5_word_groups_2_test.txt"
     with open(in_file, 'r') as file:
         text = file.read()
     n_grams = text.split('\n')
 
-    # inputs = []
-    # for x in n_grams:
-    #     inputs.append(tokenizer.encode(x, return_tensors="pt"))
-    #
-    # outputs = []
-    #
+    # append prompt
+    # prompt_prefix = "Can you tell what the encoded word is? Respond with the original passage replacing your translation of the encoded word: "
+    prompt_prefix = "Can you tell what the encoded word is? Respond with the original passage replacing your translation of the encoded word, for example if I promt 'my yodz2rMf+AuOmKOjdaAplIiyHivku2xJPOIX7y3uJQ8= is John Smith' and your guess is 'name', then return 'my name is John Smith': "
+
+    n_grams = [prompt_prefix + x for x in n_grams]
+
+    inputs = []
+    outputs = []
+
+
+    for x in n_grams:
+        inputs.append(tokenizer.encode(x, return_tensors="pt"))
+
     # for i in inputs:
     #     out = aya_model.generate(i, max_new_tokens=128)
     #     outputs.append(tokenizer.decode(out[0]))
 
     # device = torch.device("mps" if torch.mps.is_available() else "cpu")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # if torch.backends.mps.is_available:
     #     device = torch.device("mps")
-    # inputs = tokenizer(n_grams, padding=True, return_tensors="pt", truncation=True)
-    inputs = tokenizer(n_grams, padding=True, return_tensors="pt", truncation=True).input_ids.to(device)
+    # inputs = tokenizer(n_grams, padding=True, return_tensors="pt").input_ids.to(device)
 
-    with torch.no_grad():
-        outputs = aya_model.generate(inputs, max_new_tokens=128)
+    inputs2 = tokenizer(n_grams, padding=True, return_tensors="pt")
 
-    decoded_out = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    for i in inputs:
+        with torch.no_grad():
+            output = aya_model.generate(i, max_new_tokens=128)
+            outputs.append(output)
+            # output = aya_model.generate(inputs[0], max_new_tokens=128)
+            # outputs = aya_model.generate(inputs, max_new_tokens=128)
+
+    # decoded_out = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    # decoded_out = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    decoded_outs = []
+
+    for o in outputs:
+        dec_out = tokenizer.decode(o, skip_special_tokens=True)
+        decoded_outs.append(dec_out)
+        print(dec_out + "\n")
+        decoded_outs.append(dec_out)
+
 
     end = time.time()
 
     print("Time taken: ", end-start)
-    for o in outputs:
-        print(o)
+    # for o in outputs:
+    #     print(o)
+
+    # print(decoded_out)
 
     orig_filename = os.path.basename(in_file)
     orig_filename = os.path.splitext(orig_filename)[0]
     filename = "data/parsed/" + orig_filename + "_predictions.txt"
 
     with open(filename, "w") as txt_file:
-        for line in outputs:
-            txt_file.write(" ".join(line) + "\n")
+        for line in decoded_outs:
+            txt_file.write(line + "\n")
+        # txt_file.write(decoded_out)
