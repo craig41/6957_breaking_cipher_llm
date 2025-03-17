9322c8dd492b53cdd9ecd3967a3b3074f2e29656
craigblanch <craig41bl@gmail.com>
2025-03-17 12:20:23 -0600

    trying new scratch and output directories

--- aya_eval.sh
+++ aya_eval.sh
@@ -18,8 +18,8 @@
 
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 
-WORKDIR=/scratch/general/vast/$USER/CondaQA_Private
-OUTDIR=$WORKDIR/src/eval/chat-eval/llama3-3-70B/gen_edits_gen_qs/
+WORKDIR=/uufs/chpc.utah.edu/common/home/u0013114/code/6957_breaking_cipher_llm/scratch
+OUTDIR=/uufs/chpc.utah.edu/common/home/u0013114/code/6957_breaking_cipher_llm/output
 source /uufs/chpc.utah.edu/common/home/u0013114/code/6957_breaking_cipher_llm
 module load cuda/12.4.0
 
